d91e69881b984f007e5112d7d0f1842276fda78b
ANTONIO YEE <yee.antonio@gmail.com>
2013-09-21 10:17:37 -0600

    publicar lugares pendientes

--- modules/AdminModule/resources/views/lugar/index.html.php
+++ modules/AdminModule/resources/views/lugar/index.html.php
@@ -23,18 +23,33 @@
         <tr>
             <th>#</th>
             <th>Nombre</th>
+            <th>Status</th>
             <th>Acciones</th>
         </tr>
         </thead>
         <tbody>
         <?php if(empty($data)): ?>
-            <tr><td colspan="5"><p>No hay lugares registradas.</p></td></tr>
+            <tr><td colspan="4"><p>No hay lugares registradas.</p></td></tr>
         <?php else: ?>
             <?php foreach($data as $row): ?>
                 <tr>
                     <td><?=$row->getID();?></td>
                     <td><?=$view->escape($row->getNombre());?></td>
+                    <td>
+                        <?php
+                            if ( $view->escape($row->getStatus()) == '1' ) {
+                                echo 'Publico';
+                            }else{
+                                if ( $view->escape($row->getStatus()) == '2' ) {
+                                    echo 'Pendiente';
+                                }
+                            }
+                        ?>
+                    </td>
                     <td class="actions">
+                        <?php if ( $view->escape($row->getStatus()) == '2' ): ?>
+                            <a href="<?=$view['router']->generate('Admin_Lugares_Publicar', array('id' => $row->getID()));?>" title="Publicar lugar" class="btn deleteUser" data-userid="<?=$row->getID();?>"><i class="icon-remove"></i> Públicar</a>
+                        <?php endif ?>
                         <a href="<?=$view['router']->generate('Admin_Lugares_Edit', array('id' => $row->getID()));?>" title="Edit User" class="btn"><i class="icon-edit"></i> Editar</a>
                         <a href="<?=$view['router']->generate('Admin_Lugares_Delete', array('id' => $row->getID()));?>" title="Delete User" class="btn deleteUser" data-userid="<?=$row->getID();?>"><i class="icon-remove"></i> Eliminar</a>
                     </td>
