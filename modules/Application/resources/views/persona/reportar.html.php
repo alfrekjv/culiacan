15e3040f5da8b8e4b2bda6b9ad090170096954c4
Alfredo Juarez <alfrekjv@gmail.com>
2013-09-22 02:01:58 -0500

    fixed bug en reportar personas

--- modules/Application/resources/views/persona/reportar.html.php
+++ modules/Application/resources/views/persona/reportar.html.php
@@ -18,8 +18,8 @@
                 </div>
                 <div class="row">
                     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <input type="text" class="form-control" id="nombre_completo"
-                               placeholder="Escribe su nombre" name="nombre_completo" tabindex="17">
+                        <input type="text" class="form-control" id="nombre"
+                               placeholder="Escribe su nombre" name="nombre" tabindex="17">
                     </div>
 
                     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
@@ -33,37 +33,39 @@
                     </div>
                     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                         <label for="status" class="control-label">Status</label>
-                    </select>
-                </div>
-                <div class="row">
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <input type="text" class="form-control" id="edad"
-                               placeholder="Escribe su edad" name="edad" tabindex="19">
-                    </div>
-                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-                        <select id="status" name="status" class="form-control" tabindex="20">
-                            <option value="desaparecida">Desaparecida</option>
-                            <option value="en albergue">En Albergue</option>
                         </select>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <label for="mensaje" class="control-label">Observaciones</label>
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <input type="text" class="form-control" id="edad"
+                                   placeholder="Escribe su edad" name="edad" tabindex="19">
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                            <select id="status" name="status" class="form-control" tabindex="20">
+                                <option value="desaparecida">Desaparecida</option>
+                                <option value="en albergue">En Albergue</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <textarea class="form-control" id="observaciones" name="observaciones" rows="3" tabindex="21"></textarea>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <label for="mensaje" class="control-label">Observaciones</label>
+                        </div>
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <textarea class="form-control" id="observaciones" name="observaciones" rows="3"
+                                      tabindex="21"></textarea>
+                        </div>
+                    </div>
             </form>
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-danger pull-left" data-dismiss="modal" tabindex="23"><span
                     class="glyphicon glyphicon-minus-sign"></span> Cancelar
             </button>
-            <button type="button" class="btn btn-success reportarPersonaSubmit" tabindex="22"><span class="glyphicon glyphicon-ok-sign"></span>
+            <button type="button" class="btn btn-success reportarPersonaSubmit" tabindex="22"><span
+                    class="glyphicon glyphicon-ok-sign"></span>
                 Reportar
             </button>
         </div>
