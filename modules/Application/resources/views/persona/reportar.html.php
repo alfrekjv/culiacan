9e7e3a23c1d651815fd95a9ca045288e7fc1243f
Alonso Uribe <alonsouribe09@gmail.com>
2013-09-21 20:00:37 -0600

    correccion modal persona

--- modules/Application/resources/views/persona/reportar.html.php
+++ modules/Application/resources/views/persona/reportar.html.php
@@ -19,7 +19,7 @@
                 <div class="row">
                     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
                         <input type="text" class="form-control" id="nombre_completo"
-                               placeholder="Escribe su nombre completo" name="nombre_completo" tabindex="17">
+                               placeholder="Escribe su nombre" name="nombre_completo" tabindex="17">
                     </div>
 
                     <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
